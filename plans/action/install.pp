4bef10380f5a7d3b9edb2332ce9b3b9a5c8fe488
Reid Vandewiele <reid@puppet.com>
2021-06-16 15:58:53 -0700

    Change 3: replica

--- plans/action/install.pp
+++ plans/action/install.pp
@@ -21,13 +21,13 @@
 plan peadm::action::install (
   # Standard
   Peadm::SingleTargetSpec           $primary_host,
-  Optional[Peadm::SingleTargetSpec] $primary_replica_host = undef,
+  Optional[Peadm::SingleTargetSpec] $replica_host        = undef,
 
   # Large
   Optional[TargetSpec]              $compiler_hosts      = undef,
 
   # Extra Large
-  Optional[Peadm::SingleTargetSpec] $primary_postgresql_host         = undef,
+  Optional[Peadm::SingleTargetSpec] $primary_postgresql_host = undef,
   Optional[Peadm::SingleTargetSpec] $replica_postgresql_host = undef,
 
   # Common Configuration
@@ -52,16 +52,16 @@ plan peadm::action::install (
   peadm::assert_supported_pe_version($version)
 
   # Convert inputs into targets.
-  $primary_target                   = peadm::get_targets($primary_host, 1)
-  $primary_replica_target           = peadm::get_targets($primary_replica_host, 1)
-  $primary_postgresql_target         = peadm::get_targets($primary_postgresql_host, 1)
+  $primary_target            = peadm::get_targets($primary_host, 1)
+  $replica_target            = peadm::get_targets($replica_host, 1)
+  $primary_postgresql_target = peadm::get_targets($primary_postgresql_host, 1)
   $replica_postgresql_target = peadm::get_targets($replica_postgresql_host, 1)
-  $compiler_targets                 = peadm::get_targets($compiler_hosts)
+  $compiler_targets          = peadm::get_targets($compiler_hosts)
 
   # Ensure input valid for a supported architecture
   $arch = peadm::assert_supported_architecture(
     $primary_host,
-    $primary_replica_host,
+    $replica_host,
     $primary_postgresql_host,
     $replica_postgresql_host,
     $compiler_hosts,
@@ -70,14 +70,14 @@ plan peadm::action::install (
   $all_targets = peadm::flatten_compact([
     $primary_target,
     $primary_postgresql_target,
-    $primary_replica_target,
+    $replica_target,
     $replica_postgresql_target,
     $compiler_targets,
   ])
 
   $primary_targets = peadm::flatten_compact([
     $primary_target,
-    $primary_replica_target,
+    $replica_target,
   ])
 
   $database_targets = peadm::flatten_compact([
@@ -93,7 +93,7 @@ plan peadm::action::install (
 
   $agent_installer_targets = peadm::flatten_compact([
     $compiler_targets,
-    $primary_replica_target,
+    $replica_target,
   ])
 
   # Clusters A and B are used to divide PuppetDB availability for compilers
@@ -141,7 +141,7 @@ plan peadm::action::install (
   # puppet and are present in PuppetDB, it is not necessary anymore.
   $puppetdb_database_temp_config = {
     'puppet_enterprise::profile::database::puppetdb_hosts' => (
-      $compiler_targets + $primary_target + $primary_replica_target
+      $compiler_targets + $primary_target + $replica_target
     ).map |$t| { $t.peadm::certname() },
   }
 
@@ -149,7 +149,7 @@ plan peadm::action::install (
     'console_admin_password'                                          => $console_password,
     'puppet_enterprise::puppet_master_host'                           => $primary_target.peadm::certname(),
     'pe_install::puppet_master_dnsaltnames'                           => $dns_alt_names,
-    'puppet_enterprise::primary_postgresql_host'                       => $primary_postgresql_target.peadm::certname(),
+    'puppet_enterprise::primary_postgresql_host'                      => $primary_postgresql_target.peadm::certname(),
     'puppet_enterprise::profile::master::code_manager_auto_configure' => true,
     'puppet_enterprise::profile::master::r10k_remote'                 => $r10k_remote,
     'puppet_enterprise::profile::master::r10k_private_key'            => $r10k_private_key ? {
@@ -329,7 +329,7 @@ plan peadm::action::install (
         "extension_requests:${peadm::oid('pp_auth_role')}=pe_compiler",
         "extension_requests:${peadm::oid('peadm_availability_group')}=B",
       ]}
-      elsif ($target in $primary_replica_target) {[
+      elsif ($target in $replica_target) {[
         "extension_requests:${peadm::oid('peadm_role')}=puppet/server",
         "extension_requests:${peadm::oid('peadm_availability_group')}=B",
       ]}
