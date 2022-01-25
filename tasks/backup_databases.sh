5f44a0618d4f60ad7c4e3066b2eaceab76362965
David Sandilands <david@puppet.com>
2022-01-25 14:06:26 +0000

    Delete backup_databases.sh

--- tasks/backup_databases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Puppet Task Name: backup_databases
-
-datetime=
-sudo -u pe-postgres /opt/puppetlabs/server/bin/pg_dump -Fc "$PT_database" -f "${PT_directory}/${PT_database}_$(date +%Y%m%d%S).bin" || echo "Failed to dump database $PT_database"
-echo "${PT_directory}/${PT_database}_$(date +%Y%m%d%S).bin"
