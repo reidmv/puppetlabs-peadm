81f477ae4cd97dd4514aa698489eb0f458e38bf4
David Sandilands <david@puppet.com>
2022-03-16 14:48:16 +0000

    Test

--- plans/backup.pp
+++ plans/backup.pp
@@ -77,7 +77,7 @@ plan peadm::backup (
     out::message("# Backing up database ${database_names[$index]}")
     out::message("# Backing up database $cluster['primary_postgresql_host'] ")
       # If the primary postgresql host is set then pe-puppetdb needs to be remotely backed up to primary.
-      if $database_names[$index] == 'pe-puppetdb' and $cluster['primary_postgresql_host'] {
+      if $database_names[$index] == 'pe-puppetdb' {
         run_command("sudo -u pe-puppetdb /opt/puppetlabs/server/bin/pg_dump \"sslmode=verify-ca host=${cluster['primary_postgresql_host']} sslcert=/etc/puppetlabs/puppetdb/ssl/${primary_host}.cert.pem sslkey=/etc/puppetlabs/puppetdb/ssl/${primary_host}.private_key.pem sslrootcert=/etc/puppetlabs/puppet/ssl/certs/ca.pem dbname=pe-puppetdb\" -Fd -Z3 -j4 -f ${database_backup_directory}/puppetdb_$(date +%F_%T).bin" , $primary_host) # lint:ignore:140chars
         run_command("mv ${database_backup_directory}/puppetdb_*.bin ${backup_directory}/", $primary_host )
       } else {
